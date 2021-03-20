d72d516bd630d61a4334b911b037fb360d00c2d2
SuKiN-a <suryakiranpradosh@gmail.com>
2021-03-20 16:49:49 +0530

    added clause for no command repl

--- src/lib.rs
+++ src/lib.rs
@@ -125,6 +125,11 @@ impl Repl {
                 println!("Terminated REPL");
                 break;
             }
+            if self.functions.contains_key("") {
+                self.functions[&self.arguments[0]](
+                    self.arguments[0..self.arguments.len()].to_vec(),
+                );
+            }
             if self.functions.contains_key(&self.arguments[0]) {
                 self.functions[&self.arguments[0]](
                     self.arguments[1..self.arguments.len()].to_vec(),
