157707ab25e8b44ffda1c4f29e233e736c458149
SuKiN-a <suryakiranpradosh@gmail.com>
2021-04-30 22:47:03 +0530

    fixed macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,10 +1,9 @@
 #[macro_export]
 macro_rules! add_function {
     ($repl: ident, $func_name: expr, $func: ident) => {
-        use std::{any::Any, collections::HashMap, sync::Arc};
         $repl.add_function(
             $func_name.to_string(),
-            $func as fn(HashMap<String, Arc<dyn Any>>, Vec<String>),
+            $func as fn(std::collections::HashMap<String, std::sync::Arc<dyn std::any::Any>>, Vec<String>),
         );
     };
 }
